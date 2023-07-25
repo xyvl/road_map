0d87c0fdff97b6dd9724d14c218095a0a161c6aa
Vlad Podshivalov <23091980luba@gmail.com>
2023-07-25 19:35:15 +0500

    others end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,31 @@
-import { Backend } from "@/components/backend/Backend"
-import { Blockchain } from "@/components/blockchain/Blockchain"
-import { Frontend } from "@/components/frontend/Frontend"
-import { Mobile } from "@/components/mobile/Mobile"
+import { Backend } from "@/components/backend/Backend";
+import { Blockchain } from "@/components/blockchain/Blockchain";
+import { Frontend } from "@/components/frontend/Frontend";
+import { Mobile } from "@/components/mobile/Mobile";
+import { OthersSkills } from "@/components/othersSkills/OthersSkills"
 
 export default function Home() {
   return (
     <div className="wrapper">
       <div>
         <h1 className="technology">⛓ Blockchain:</h1>
-        <Blockchain/>
+        <Blockchain />
       </div>
       <div>
         <h1 className="technology">🌝 Frontend:</h1>
-        <Frontend/>
+        <Frontend />
       </div>
       <div>
         <h1 className="technology">🌚 Backend:</h1>
-        <Backend/>
+        <Backend />
       </div>
       <div>
         <h1 className="technology">📱 Mobile:</h1>
-<Mobile/>
+        <Mobile />
       </div>
       <div>
         <h1 className="technology">✍ Другие скиллы:</h1>
-
+        <OthersSkills/>
       </div>
     </div>
   );
---
+++ src/components/othersSkills/OthersSkills.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const OthersSkills = () => {
+  // 	— Git / GitHub / GitLab
+  // — SOLID / ООП
+  // — Docker
+  // — JSON
+  return (
+    <>
+      <div>
+        <span className="text technology">— </span>
+        <span className="text yes">Git</span>
+        <span className="text technology"> / </span>
+        <span className="text yes">GitHub</span>
+      </div>
+      <div>
+        <span className="text technology">— </span>
+        <span className="text yes">SOLID</span>
+        <span className="text technology"> / </span>
+        <span className="text yes">ООП</span>
+      </div>
+      <div>
+        <span className="text technology">— </span>
+        <span className="text yes">JSON</span>
+      </div>
+    </>
+  );
+};

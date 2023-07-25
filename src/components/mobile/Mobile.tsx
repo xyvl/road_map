c8731d0dd6dca290f99b27dd42ad5dd52702ae52
Vlad Podshivalov <23091980luba@gmail.com>
2023-07-25 21:47:44 +0500

    Mobile finish

--- src/components/mobile/Mobile.tsx
+++ src/components/mobile/Mobile.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import React from "react";
 
 export const Mobile = () => {
@@ -5,11 +6,11 @@ export const Mobile = () => {
     <>
       <div>
         <span className="text technology">— </span>
-        <span className="text yes">React Native</span>
+        <Link href="https://youtu.be/K-aD8ClG1FQ" className="text yes">React Native</Link>
         <span className="text technology"> / </span>
-        <span className="text yes">Expo cli</span>
+        <Link href="https://www.jscamp.app/ru/docs/reactnative016" className="text yes">Expo cli</Link>
         <span className="text technology"> / </span>
-        <span className="text yes">React Native cli</span>
+        <Link href="https://habr.com/ru/companies/neoflex/articles/428912/" className="text yes">React Native cli</Link>
       </div>
     </>
   );

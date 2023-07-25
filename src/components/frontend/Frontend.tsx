3dae54cf9589f0a18471451895c74f33f696a302
Vlad Podshivalov <23091980luba@gmail.com>
2023-07-25 20:43:03 +0500

    frontend finish

--- src/components/frontend/Frontend.tsx
+++ src/components/frontend/Frontend.tsx
@@ -1,82 +1,138 @@
+import Link from "next/link";
 import React from "react";
 
 export const Frontend = () => {
   return (
     <>
       <div>
-        <span className="text technology">— </span>
-        <span className="text yes">TypeScript</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">JavaScript</span>
+        <span className="technology">— </span>
+        <Link
+          href="https://youtube.com/playlist?list=PLu6MFGxDdilhKzUePH96oqhedQXROTNmg"
+          className="yes"
+        >
+          TypeScript
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/CxgOKJh4zWE" className="yes">
+          JavaScript
+        </Link>
       </div>
       <div>
-        <span className="text technology">— </span>
-        <span className="text yes">NextJS</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">ReactJS</span>
+        <span className="technology">— </span>
+        <Link
+          href="https://youtube.com/playlist?list=PLiZoB8JBsdzlgeYHZDJ_orG0vy8JiEhKr"
+          className="yes"
+        >
+          NextJS
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/GNrdg3PzpJQ" className="yes">
+          ReactJS
+        </Link>
       </div>
       <div>
-        <span className="text technology">— </span>
-        <span className="text yes">Redux Toolkit</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">Redux</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">Redux Saga</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">Redux Thunk</span>
+        <span className="technology">— </span>
+        <Link
+          href="https://youtube.com/playlist?list=PLiZoB8JBsdznQv3kAEvTzDP2qjaUI8Vo7"
+          className="yes"
+        >
+          Redux Toolkit
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/z-u0tDuKS2o" className="no">
+          Redux
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/Rq-sIE2ceEU" className="no">
+          MobX
+        </Link>
+        <span className="technology"> / </span>
+        <Link
+          href="https://youtube.com/playlist?list=PLNkWIWHIRwMFrVMaVMOYflo8R4QBk4vk0"
+          className="no"
+        >
+          Redux Saga
+        </Link>
       </div>
-			<div>
-        <span className="text technology">— </span>
-        <span className="text yes">React Query</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">RTK Query</span>
+      <div>
+        <span className="technology">— </span>
+        <Link href="https://youtu.be/4-SOv7eTfoQ" className="yes">
+          React Query
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/uSwe-5dPrV8" className="no">
+          RTK Query
+        </Link>
       </div>
-			<div>
-        <span className="text technology">— </span>
-        <span className="text yes">SCSS</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">styled-components</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">CSS Modules</span>
+      <div>
+        <span className="technology">— </span>
+        <Link href="https://sass-scss.ru/guide/" className="yes">
+          SCSS
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/vp9h7CvZuaA" className="yes">
+          styled-components
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/PXPrIK2XPmQ" className="yes">
+          CSS Modules
+        </Link>
       </div>
-			<div>
-        <span className="text technology">— </span>
-        <span className="text yes">Material UI</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">Semantic UI</span>
-        <span className="text technology"> / </span>
-        <span className="text yes">Bootstrap</span>
+      <div>
+        <span className="technology">— </span>
+        <Link href="https://youtu.be/OZ_FUaV_xbg" className="no">
+          Material UI
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/5WeDOhff2pY" className="no">
+          React Bootstrap
+        </Link>
       </div>
-			<div>
-        <span className="text technology">— </span>
-        <span className="text yes">Webpack</span>
+      <div>
+        <span className="technology">— </span>
+        <Link href="https://youtu.be/uNLaS0S2FmU" className="no">
+          Webpack
+        </Link>
       </div>
-			<div>
-        <span className="text technology">— </span>
-        <span className="text yes">React Testing Library</span>
-				<span className="text technology"> / </span>
-        <span className="text yes">Jest + Enzyme</span>
-				<span className="text technology"> / </span>
-        <span className="text yes">Puppeteer</span>
-				<span className="text technology"> / </span>
+      <div>
+        <span className="technology">— </span>
+        <Link href="https://youtu.be/sjdMBJ72M4s" className="no">
+          React Testing Library
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/y2emL1fMRyY" className="no">
+          Jest
+        </Link>
       </div>
-			<div>
-        <span className="text technology">— </span>
-        <span className="text yes">SSR</span>
-				<span className="text technology"> / </span>
-        <span className="text yes">Profiler</span>
-				<span className="text technology"> / </span>
-        <span className="text yes">Optimizing Perfomance</span>
-				<span className="text technology"> / </span>
-        <span className="text yes">Code Splitting</span>
-				<span className="text technology"> / </span>
-        <span className="text yes">Lazy Loading</span>
+      <div>
+        <span className="technology">— </span>
+        <Link href="https://youtu.be/EH1WsQGSrWU" className="yes">
+          SSR
+        </Link>
+        <span className="technology"> / </span>
+        <Link
+          href="https://ru.legacy.reactjs.org/docs/optimizing-performance.html"
+          className="yes"
+        >
+          Optimizing Perfomance
+        </Link>
+        <span className="technology"> / </span>
+        <Link
+          href="https://ru.legacy.reactjs.org/docs/code-splitting.html#:~:text=%D0%A0%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BA%D0%BE%D0%B4%D0%B0%20%E2%80%94%20%D1%8D%D1%82%D0%BE%20%D0%B2%D0%BE%D0%B7%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C%2C%20%D0%BF%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%B8%D0%B2%D0%B0%D0%B5%D0%BC%D0%B0%D1%8F,%D0%B7%D0%B0%D0%B3%D1%80%D1%83%D0%B6%D0%B0%D1%82%D1%8C%20%D0%B8%D1%85%20%D0%BF%D0%BE%20%D0%BC%D0%B5%D1%80%D0%B5%20%D0%BD%D0%B5%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8."
+          className="yes"
+        >
+          Code Splitting
+        </Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/ANQYKGUcLMg"  className="yes">
+          Lazy Loading
+        </Link>
       </div>
-			<div>
-        <span className="text technology">— </span>
-        <span className="text yes">ESLint</span>
-				<span className="text technology"> / </span>
-        <span className="text yes">Prettier</span>
+      <div>
+        <span className="technology">— </span>
+        <Link href="https://youtu.be/RXaltL8yIlc" className="no">ESLint</Link>
+        <span className="technology"> / </span>
+        <Link href="https://youtu.be/Obz-gy5E_zo" className="no">Prettier</Link>
       </div>
     </>
   );
